1f40b488dea479769b7c5db4de3a7586f83a7f18
Maryam Hussain <202011514@stu.uob.edu.bh>
2026-09-10 10:36:14 +0300

    fixed Nav Bar issue

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,28 +3,44 @@ import { Link } from 'react-router';
 import { UserContext } from '../../contexts/UserContext';
 
 const NavBar = () => {
+  const { user, setUser } = useContext(UserContext);
 
-  const { user, setUser } = useContext(UserContext)
-
-  const handleSignOut = ()=>{
-    localStorage.removeItem('token')
-    setUser(null)
-  }
+  const handleSignOut = () => {
+    localStorage.removeItem('token');
+    setUser(null);
+  };
 
   return (
     <nav>
       {user ? (
         <ul>
-          <li><Link to='/'>HOME</Link></li>
-          <li><Link to='/hoots'>HOOTS</Link></li>
-          <li><Link to='/' onClick={handleSignOut}>Sign Out</Link></li>
+          <li>
+            <Link to='/'>HOME</Link>
+          </li>
+          <li>
+            <Link to='/hoots'>HOOTS</Link>
+          </li>
+          <li>
+            <Link to='/' onClick={handleSignOut}>
+              Sign Out
+            </Link>
+          </li>
         </ul>
       ) : (
         <ul>
-          <li><Link to='/'>HOME</Link></li>
-          <li><Link to='/sign-in'>SIGN IN</Link></li>
-          <li><Link to='/sign-up'>SIGN UP</Link></li>
+          <li>
+            <Link to='/'>HOME</Link>
+          </li>
+          <li>
+            <Link to='/sign-in'>SIGN IN</Link>
+          </li>
+          <li>
+            <Link to='/sign-up'>SIGN UP</Link>
+          </li>
         </ul>
       )}
     </nav>
   );
+};
+
+export default NavBar;
