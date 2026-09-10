8c9df1a9653cf4bd0c9db0bd273abe8bc6e667bf
Maryam Hussain <202011514@stu.uob.edu.bh>
2026-09-10 10:38:54 +0300

    added routes in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,35 @@ import { Route, Routes } from 'react-router';
 import NavBar from './components/NavBar/NavBar';
 import SignUpForm from './components/SignUpForm/SignUpForm';
 import SignInForm from './components/SignInForm/SignInForm';
-import Dashboard from './components/Dashboard/Dashboard'
-import Landing from './components/Landing/Landing'
+import Dashboard from './components/Dashboard/Dashboard';
+import Landing from './components/Landing/Landing';
+import HootList from './components/HootList/HootList';
 
 // Context
 import { UserContext } from './contexts/UserContext';
 
 const App = () => {
-  const { user } = useContext(UserContext)
+  const { user } = useContext(UserContext);
 
   return (
     <>
       <NavBar />
+
       <Routes>
-        <Route path='/' element={user ? <Dashboard /> : <Landing/> } />
-        <Route path='/sign-up' element={<SignUpForm />} />
-        <Route path='/sign-in' element={<SignInForm />} />
+        <Route path='/' element={user ? <Dashboard /> : <Landing />} />
+
+        {user ? (
+          <>
+            {/* Protected routes */}
+            <Route path='/hoots' element={<HootList />} />
+          </>
+        ) : (
+          <>
+            {/* Non-user routes */}
+            <Route path='/sign-up' element={<SignUpForm />} />
+            <Route path='/sign-in' element={<SignInForm />} />
+          </>
+        )}
       </Routes>
     </>
   );
