24bcd8b8f26b11ae5ed9e7c8e87c6c900259ac01
Maryam Hussain <202011514@stu.uob.edu.bh>
2026-09-10 10:45:54 +0300

    updated the app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router';
 
 // Components
@@ -9,29 +9,59 @@ import Dashboard from './components/Dashboard/Dashboard';
 import Landing from './components/Landing/Landing';
 import HootList from './components/HootList/HootList';
 
+// Services
+import * as hootService from './services/hootService';
+
 // Context
 import { UserContext } from './contexts/UserContext';
 
 const App = () => {
   const { user } = useContext(UserContext);
 
+  const [hoots, setHoots] = useState([]);
+
+  useEffect(() => {
+    const fetchAllHoots = async () => {
+      const hootsData = await hootService.index();
+
+      console.log('hootsData:', hootsData);
+
+      setHoots(hootsData);
+    };
+
+    if (user) fetchAllHoots();
+  }, [user]);
+
   return (
     <>
       <NavBar />
 
       <Routes>
-        <Route path='/' element={user ? <Dashboard /> : <Landing />} />
+        <Route
+          path='/'
+          element={user ? <Dashboard /> : <Landing />}
+        />
 
         {user ? (
           <>
             {/* Protected routes */}
-            <Route path='/hoots' element={<HootList />} />
+            <Route
+              path='/hoots'
+              element={<HootList hoots={hoots} />}
+            />
           </>
         ) : (
           <>
             {/* Non-user routes */}
-            <Route path='/sign-up' element={<SignUpForm />} />
-            <Route path='/sign-in' element={<SignInForm />} />
+            <Route
+              path='/sign-up'
+              element={<SignUpForm />}
+            />
+
+            <Route
+              path='/sign-in'
+              element={<SignInForm />}
+            />
           </>
         )}
       </Routes>
