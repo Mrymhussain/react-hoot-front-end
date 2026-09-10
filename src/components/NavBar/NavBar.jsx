fe37d45250d8e670bf818cb34a06d82dbc91ff77
Maryam Hussain <202011514@stu.uob.edu.bh>
2026-09-10 10:33:54 +0300

    Nav Bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -13,25 +13,18 @@ const NavBar = () => {
 
   return (
     <nav>
-      <ul>
-
-        { user
-          ?
-          <>
-            <li>Hello {user.username}</li>
-            <li><Link to="/">Dashboard</Link></li>
-            <li><Link to="/" onClick={handleSignOut}>Sign Out</Link></li>
-          </>
-          :
-          <>
-            <li><Link to="/">Dashboard</Link></li>
-            <li><Link to='/sign-up'>Sign Up</Link></li>
-            <li><Link to='/sign-in'>Sign In</Link></li>
-          </>
-        }
-      </ul>
+      {user ? (
+        <ul>
+          <li><Link to='/'>HOME</Link></li>
+          <li><Link to='/hoots'>HOOTS</Link></li>
+          <li><Link to='/' onClick={handleSignOut}>Sign Out</Link></li>
+        </ul>
+      ) : (
+        <ul>
+          <li><Link to='/'>HOME</Link></li>
+          <li><Link to='/sign-in'>SIGN IN</Link></li>
+          <li><Link to='/sign-up'>SIGN UP</Link></li>
+        </ul>
+      )}
     </nav>
   );
-};
-
-export default NavBar;
